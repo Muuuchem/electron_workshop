0de80a625d829a82f9f6cf622095d85f94f7e9da
Tyler Eakes <teakesprofessional@gmail.com>
2017-07-22 12:58:28 -0700

    text editor working

---
+++ index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } =
+require('electron');
+
+app.on('ready', () => {
+
+  const mainWindow = new BrowserWindow({});
+
+  const file = 'file://' + __dirname + '/index.html';
+
+  mainWindow.loadURL(file);
+
+  mainWindow.webContents.on('will-navigate', (e, url) => {
+    e.preventDefault();
+    console.log(url);
+
+    const realPath = url.slice(7);
+    mainWindow.webContents.send('open-file', realPath);
+  });
+
+});
